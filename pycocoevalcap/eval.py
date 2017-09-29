1dcd7bdd651b63e9aca803fea82eb194aa75eeef
Ondrej Dusek <o.dusek@hw.ac.uk>
2017-09-29 17:34:01 +0100

    Not computing COCO BLEU since it's confusing

--- pycocoevalcap/eval.py
+++ pycocoevalcap/eval.py
@@ -36,7 +36,6 @@ class COCOEvalCap:
         # =================================================
         print 'setting up scorers...'
         scorers = [
-            (Bleu(4), ["Bleu_1", "Bleu_2", "Bleu_3", "Bleu_4"]),
             (Meteor(),"METEOR"),
             (Rouge(), "ROUGE_L"),
             (Cider(), "CIDEr")
