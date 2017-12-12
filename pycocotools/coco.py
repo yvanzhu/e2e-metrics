7e432039aa25110b31d570b1d6079493368497cf
Ondrej Dusek <o.dusek@hw.ac.uk>
2017-12-12 17:08:36 +0000

    Typo

--- pycocotools/coco.py
+++ pycocotools/coco.py
@@ -114,7 +114,7 @@ class COCO:
         Print information about the annotation file.
         :return:
         """
-        for key, value in self.datset['info'].items():
+        for key, value in self.dataset['info'].items():
             print >> sys.stderr, '%s: %s'%(key, value)
 
     def getAnnIds(self, imgIds=[], catIds=[], areaRng=[], iscrowd=None):
